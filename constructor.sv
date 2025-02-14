1d46384879459205e93330c124d1f1d5685fb80a
NoufSaed <nofsaed02@gmail.com>
2025-02-14 18:19:09 +0300

    Update constructor.sv

--- constructor.sv
+++ constructor.sv
@@ -1,1 +1,29 @@
+class counter;
+    protected int count;
+    
+
+    function new(int initial_value = 0);
+        count = initial_value;
+    endfunction
+    
+    function void load(int value);
+        count = value;
+    endfunction
+    
+    function int getcount();
+        return count;
+    endfunction
+endclass
+
+module test;
+    initial begin
+        counter c1, c2;
+      
+        c1 = new();
+        $display("Default counter value: %0d", c1.getcount());
+        
+        c2 = new(100);
+        $display("Counter value with initial value 100: %0d", c2.getcount());
+    end
+endmodule
 
