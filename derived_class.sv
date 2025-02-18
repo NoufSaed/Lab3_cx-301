e26ebd48301c3404faa4c69bbe5e3f2d64ffec2a
NoufSaed <nofsaed02@gmail.com>
2025-02-18 11:30:53 +0300

    Lab3

--- derived_class.sv
+++ derived_class.sv
@@ -1,52 +1,37 @@
-class counter;
-    protected int count;
-    
-    function new(int initial_value = 0);
-        count = initial_value;
+class upcounter extends counter;
+    bit carry;
+    function new(int value = 0, int max_val = 10, int min_val = 0);
+        super.new(value, max_val, min_val);
+        carry = 0;
     endfunction
-    
-    function void load(int value);
-        count = value;
-    endfunction
-    
-    function int getcount();
-        return count;
+
+    function void next();
+        if (count == max) begin
+            count = min;
+            carry = 1;
+        end else begin
+            count++;
+            carry = 0;
+        end
+        $display("UpCounter: %0d (Carry: %b)", count, carry);
     endfunction
 endclass
-// ..Upcounter .. 
-class upcounter extends counter ;
-// pass the value to counter class 
-  function new (int value =0);
-    super.new(value);
-  endfunction 
-// to count
-  function void next();
-    count ++;
-    $display("Upcounter :%d",count);
-  endfunction
 
-endclass 
-//..downcounter ..
-class downcounter extends counter ;
-
-  function new (int value = 0);
-    super.new (value) 
-  endfunction
-
-  function void  next();
-    count--;
-    $display("Downcounter :%d",count)  ;
+class downcounter extends counter;
+    bit borrow;
+    function new(int value = 0, int max_val = 10, int min_val = 0);
+        super.new(value, max_val, min_val);
+        borrow = 0;
     endfunction
-endclass 
 
-module test ;
-initial begin
-        upcounter up1;
-        downcounter down1;
-        
-        up1 = new(5);
-        down1 = new(10);
-        repeat(3) up1.next();
-        repeat(3) down1.next();
-end 
-endmodule
+    function void next();
+        if (count == min) begin
+            count = max;
+            borrow = 1;
+        end else begin
+            count--;
+            borrow = 0;
+        end
+        $display("DownCounter: %0d (Borrow: %b)", count, borrow);
+    endfunction
+endclass
