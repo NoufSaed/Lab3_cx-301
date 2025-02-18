9070c7acf27502c9919f1da22d5402a29e1cc67b
NoufSaed <nofsaed02@gmail.com>
2025-02-18 11:30:03 +0300

    Lab3

--- simple_class.sv
+++ simple_class.sv
@@ -1,24 +1,24 @@
-class counter;
-	protected int count ;
+virtual class counter;
+    protected int count;
+    protected int max, min;
 
-	function void load (int value);
-		count =value ;
-	endfunction 
+    function new(int value = 0, int max_val = 10, int min_val = 0);
+        count = value;
+        max = max_val;
+        min = min_val;
+    endfunction
 
-	function int getcount  ();
-		return count ;
-	endfunction
+    function void load(int value);
+        if (value > max) count = max;
+        else if (value < min) count = min;
+        else count = value;
+    endfunction
 
+    function int getcount();
+        return count;
+    endfunction
+
+    virtual function void next();
+        $display("Counter: next() called from base class");
+    endfunction
 endclass
-
-module test ;
-initial begin
-	counter c1 ;
-	c1 =new();
-	c1.load(9);
-	$display( "The counter After loading :%d ",c1.getcount());
-	 
-	c1.load(10);
-	$display ("The counter After loading",c1.getcount());
-end 
-endmodule 
