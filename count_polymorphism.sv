bf1dad06854a5f0ed7d8adfed1c076efd3b1263c
NoufSaed <nofsaed02@gmail.com>
2025-02-18 11:33:23 +0300

    Lab3

--- count_polymorphism.sv
+++ count_polymorphism.sv
@@ -1,1 +1,16 @@
+module count_polymorphism;
+    initial begin
+        counter c;
+        upcounter u = new(9, 10, 0);
+        downcounter d = new(1, 10, 0);
+        upcounter u_new;
 
+        $display("=== Testing Polymorphism ===");
+        c = u;
+        c.next();
+
+        $display("=== Testing Type Casting ===");
+        $cast(u_new, c);
+        u_new.next();
+    end
+endmodule
